24617ab534c63bdf1ad2863d4a95b76bc219eca7
johnston <johnston>
2001-03-27 17:38:10 +0000

    adding vector.h intermediary to undef min and max

---
+++ src/include/ivstd/vector.h
@@ -0,0 +1,10 @@
+#ifndef _iv_vector_h
+#define _iv_vector_h
+#define _min_save min
+#define _max_save max
+#undef min
+#undef max
+#include_next <vector.h>
+#define min _min_save
+#define max _max_save
+#endif
